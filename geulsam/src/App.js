54d6d22934c1fceffcd1591cfafcb65cefde768b
DeusJae <106643398+DeusJae@users.noreply.github.com>
2024-07-07 17:40:41 +0900

    Book Upload
    문집 업로드

--- geulsam/src/App.js
+++ geulsam/src/App.js
@@ -11,6 +11,7 @@ import LoginLayout from './components/Layouts/LoginLayout';
 import Book from './pages/archive/book/Book';
 import BookInfo from './pages/archive/book/BookInfo';
 import PosterUpload from './pages/archive/poster/PosterUpload';
+import BookUpload from './pages/admin/upload/book/BookUpload';
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
       <Routes>
         <Route element={<NavLayout />}>
           <Route path='/poster/upload' element={<PosterUpload />} />
-
+          <Route path='/book/upload' element={<BookUpload />} />
           <Route path='/frontend' element={<Main />} />
           <Route path='/' element={<Main />} />
           <Route path='/main' element={<Main />} />
--- geulsam/src/pages/admin/upload/book/bookUpload.jsx
+++ geulsam/src/pages/admin/upload/book/bookUpload.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
+import { useState } from 'react';
+import { useForms } from '../../../../hooks/useForms';
+import { Input, Inputs, Form, InputTitle, Button, BookInfoContainer, BookTitle, InputUploads, RightSubmit } from '../../../../style/StyledComponent';
 
-const bookUpload = () => {
+const BookUpload = () => {
+    const [title, onChangeTitle] = useForms();
+    const [release, onChangeRelease] = useForms();
+    const [designer, onChangeDesigner] = useForms();
+    const [plate, onChangePlate] = useForms();
+    const [pageNumber, onChangePageNumber] = useForms();
+    const [bookCover, setBookCover] = useState(null);
+    const [pdf, setPdf] = useState(null);
+    const [year, onChangeYear] = useForms();
+
+    const onFileChange = (e) => {
+
+        setBookCover(e.target.files[0]);
+        console.log(bookCover)
+    };
+
     return (
-        <div>
-
-        </div>
+        <BookInfoContainer>
+            <BookTitle>
+                새 문집 게시하기
+            </BookTitle>
+            <Inputs>
+                <div>
+                    <InputTitle>제목</InputTitle>
+                    <Input value={title} onChange={onChangeTitle} placeholder='예)와우문집2024' />
+                </div>
+                <div>
+                    <InputTitle>발간일</InputTitle>
+                    <Input value={release} onChange={onChangeRelease} placeholder='예)2024' />
+                </div>
+                <div>
+                    <InputTitle>디자인</InputTitle>
+                    <Input value={designer} onChange={onChangeDesigner} placeholder='예) 아이묭' />
+                </div>
+                <div>
+                    <InputTitle>판형</InputTitle>
+                    <Input value={plate} onChange={onChangePlate} placeholder='예)B6' />
+                </div>
+                <div>
+                    <InputTitle>쪽수</InputTitle>
+                    <Input value={pageNumber} onChange={onChangePageNumber} placeholder='예)370' />
+                </div>
+                <div>
+                    <InputTitle>표지 및 내지 게시</InputTitle>
+                    <p>정상적인 문집 게시를 위해 하단의 업로드 가이드라인을 준수해주세요.</p>
+                    <p>게시에 성공할 시 하단에 이미지가 표시됩니다.</p>
+                    <Input type='file' onChange={onFileChange} />
+                </div>
+            </Inputs>
+        </BookInfoContainer>
     );
 };
 
-export default bookUpload;
+export default BookUpload;
