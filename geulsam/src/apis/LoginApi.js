d18fa62adad9a01f380137bc97c16ee5d476f82d
yam <1669jsh@naver.com>
2024-07-31 17:42:44 +0900

    주소 수정
    api 수정

--- geulsam/src/apis/LoginApi.js
+++ geulsam/src/apis/LoginApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const LoginApi = async (schoolNum, password) => {
-    const result = await axios.post('http://43.200.215.113:8080/user/login', { schoolNum, password });
+    const result = await axios.post('https://geulsaem.store/user/login', { schoolNum, password });
     console.log(result)
     return result;
 }
