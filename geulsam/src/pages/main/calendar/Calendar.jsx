3a9056502f5f259c10d600a9ddcedf46cb161761
yam <1669jsh@naver.com>
2024-08-01 10:04:23 +0900

    index.html
    경로수정

--- geulsam/src/pages/main/calendar/Calendar.jsx
+++ geulsam/src/pages/main/calendar/Calendar.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Calendars, CalendarTitle } from '../../../style/StyledComponent';
 import CenterMode from '../../../components/Carousel/Slides';
-import axios from 'axios';
 import { normalAPI } from '../../../apis/Api';
 
 
