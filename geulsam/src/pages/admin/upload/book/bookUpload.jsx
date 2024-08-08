a68ed744ac6bc1ee612a3c40a97bcc33a4c94f23
DeusJae <106643398+DeusJae@users.noreply.github.com>
2024-08-09 01:36:45 +0900

    BookCover BackCover Thumbnail 추가
    BookCover BackCover Thumbnail 추가

--- geulsam/src/pages/admin/upload/book/bookUpload.jsx
+++ geulsam/src/pages/admin/upload/book/bookUpload.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { useState } from 'react';
 import { useForms } from '../../../../hooks/useForms';
 import { Input, Inputs, Form, InputTitle, Button, BookInfoContainer, BookTitle, InputUploads, RightSubmit } from '../../../../style/StyledComponent';
+import Resizer from "react-image-file-resizer"
+import axios from 'axios';
+const EndPoint = "http://43.200.215.113:8080/book";
 
 const BookUpload = () => {
     const [title, onChangeTitle] = useForms();
@@ -10,14 +13,163 @@ const BookUpload = () => {
     const [plate, onChangePlate] = useForms();
     const [pageNumber, onChangePageNumber] = useForms();
     const [bookCover, setBookCover] = useState(null);
+    const [bookCoverThumbnail,setBookCoverThumbnail] = useState();
+    const [bookCoverUrl,setBookCoverUrl] = useState();
+    const [backCover,setBackCover] = useState(null);
+    const [backCoverUrl,setBackCoverUrl] = useState();
+    const [backCoverThumbnail,setBackCoverThumbnail] = useState();
     const [pdf, setPdf] = useState(null);
     const [year, onChangeYear] = useForms();
+    
 
-    const onFileChange = (e) => {
+    // {
+    //     "bookCover": "string",
+    //     "bookCoverThumbnail": "string",
+    //     "backCover": "string",
+    //     "backCoverThumbnail": "string",
+    //     "pdf": "string",
+    //     "designer": "string",
+    //     "plate": "string",
+    //     "pageNumber": 0,
+    //     "year": 0,
+    //     "release": "2024-08-08",
+    //     "title": "string"
+    //   }
 
-        setBookCover(e.target.files[0]);
-        console.log(bookCover)
+    const resizeFile = (file) =>
+        new Promise((resolve) => {
+            Resizer.imageFileResizer(
+                file,
+                700,
+                700,
+                "JPEG",
+                80,
+                0,
+                (uri) => {
+                    resolve(uri);
+                },
+                "file"
+            );
+        });
+
+    const onBookCoverChange = async (e) => {
+        try {
+            const selectedBookCoverFile = e.target.files[0]
+            setBookCover(selectedBookCoverFile);
+
+            const resizedBookCover = await resizeFile(selectedBookCoverFile)
+            setBookCoverThumbnail(resizedBookCover)
+
+            const readerBookCover = new FileReader();
+            readerBookCover.onload = () => {
+                setBookCoverUrl(readerBookCover.result);
+            };
+            readerBookCover.readAsDataURL(resizedBookCover);
+
+            console.log(selectedBookCoverFile)
+            console.log(resizedBookCover)
+        } catch (err) {
+            console.log(err)
+        }
     };
+    const onBackCoverChange = async (e) => {
+        try {
+            const selectedBackCoverFile = e.target.files[0]
+            setBookCover(selectedBackCoverFile);
+
+            const resizedBackCover = await resizeFile(selectedBackCoverFile)
+            setBookCoverThumbnail(resizedBackCover)
+
+            const readerBackCover = new FileReader();
+            readerBackCover.onload = () => {
+                setBackCoverUrl(readerBackCover.result);
+            };
+            readerBackCover.readAsDataURL(resizedBackCover);
+
+            console.log(selectedBackCoverFile)
+            console.log(resizedBackCover)
+        } catch (err) {
+            console.log(err)
+        }
+    };
+
+    const onClickUpload = async (e) => {
+        const suppertedFormats = ["image/jpeg", "image/png", "image/svg+xml"]
+        e.preventDefault();
+        if (!bookCover||!backCover) {
+            alert("파일을 선택해주세요.");
+            return;
+        }
+        if (!suppertedFormats.includes(bookCover.type)||!suppertedFormats.includes(backCover.type)) {
+            alert("지원되지 않은 이미지 형식입니다. JPEG, PNG형식의 이미지를 업로드해주세요.");
+            return;
+        }
+
+
+
+        const formData = new FormData();
+        formData.append('bookCover', bookCover)
+        formData.append('bookCoverThumbNail', bookCoverThumbnail)
+        formData.append('backCover', year);
+        formData.append('designer', designer);
+        formData.append("plate",plate);
+        formData.append("pageNumber",pageNumber);
+        formData.append("year",year);
+        formData.append("release",release);
+        formData.append("title",title);
+
+    // {
+    //     "bookCover": "string",
+    //     "bookCoverThumbnail": "string",
+    //     "backCover": "string",
+    //     "backCoverThumbnail": "string",
+    //     "pdf": "string",
+    //     "designer": "string",
+    //     "plate": "string",
+    //     "pageNumber": 0,
+    //     "year": 0,
+    //     "release": "2024-08-08",
+    //     "title": "string"
+    //   }
+
+
+        const accessToken = localStorage.getItem('access')
+
+        try {
+            const refreshToken = localStorage.getItem('refresh');
+
+            //처음으로 업로드시
+            const res = await axios.post(EndPoint, formData, {
+                headers: {
+
+                    'Content-Type': 'multipart/form-data',
+                    'accessToken': accessToken,
+                },
+            })
+            console.log(res)
+        } catch (error) {
+            // 에러 발생
+            console.error(error);
+            try {
+                //리프레쉬 토큰 포함해서 다시 전송
+                const refreshToken = localStorage.getItem('refresh');
+
+                const res = await axios.post(EndPoint, formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        'refreshToken': refreshToken,
+                    },
+                })
+                console.log(res)
+            } catch (err) {
+                //그래도 안되면 재로그인 요청
+                console.log(err)
+                alert('다시 로그인 해주세요.')
+            }
+        }
+    }
+
+
 
     return (
         <BookInfoContainer>
@@ -48,8 +200,28 @@ const BookUpload = () => {
                 <div>
                     <InputTitle>표지 및 내지 게시</InputTitle>
                     <p>정상적인 문집 게시를 위해 하단의 업로드 가이드라인을 준수해주세요.</p>
-                    <p>게시에 성공할 시 하단에 이미지가 표시됩니다.</p>
-                    <Input type='file' onChange={onFileChange} />
+                    <p>게시에 성공할 시 하단에 이미지가 표시됩니다. </p>
+                    <br/>
+                    <InputTitle>앞표지</InputTitle>
+                    <Input type='file' onChange={onBookCoverChange} />
+                </div>
+                {bookCoverUrl && (
+                <div>
+                    <img src={bookCoverUrl} alt="Thumbnail"/>
+                </div>)}
+                <div>
+                    <br/>
+                    <InputTitle>뒷표지</InputTitle>
+                    <Input type='file' onChange={onBackCoverChange} />
+                </div>
+                {backCoverUrl && (
+                    <div>
+                        <img src={backCoverUrl} alt="Thumbnail"/>
+                    </div>
+                )}
+                <div>
+                    <InputTitle>본문</InputTitle>
+                    <Input type='file' onChange={setPdf} />
                 </div>
             </Inputs>
         </BookInfoContainer>
