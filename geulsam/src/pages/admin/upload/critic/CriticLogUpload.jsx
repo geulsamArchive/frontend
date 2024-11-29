9caa22198b536a73f84df510252ca3790c7143c6
yam <1669jsh@naver.com>
2024-11-29 17:21:26 +0900

    크리틱 로그, 글 업로드 수정
    select tag option 수정

--- geulsam/src/pages/admin/upload/critic/CriticLogUpload.jsx
+++ geulsam/src/pages/admin/upload/critic/CriticLogUpload.jsx
@@ -1,139 +1,152 @@
 import React from 'react';
 import { useForms } from '../../../../hooks/useForms';
 import { normalAPI } from '../../../../apis/Api';
-import { BookInfoContainer, BookTitle, Button, LoginInput } from '../../../../style/StyledComponent';
+import {
+  BookInfoContainer,
+  BookTitle,
+  Button,
+  LoginInput,
+} from '../../../../style/StyledComponent';
 import { VisibleSelect } from '../../../../style/WokrUpload';
-import { CriticLogUploads, CriticLogUploadsSecondLine } from '../../../../style/admin/critic';
+import {
+  CriticLogUploads,
+  CriticLogUploadsSecondLine,
+} from '../../../../style/admin/critic';
 import { useAuth } from '../../../../store/Auth';
 
-
 const CriticLogUpload = () => {
-    const [title, onChangeTitle] = useForms();
-    const [date, onChangeDate] = useForms();
-    const [name, onChangeName] = useForms();
-    const [genre, onChangeGenre] = useForms();
-    const [url, onChangeUrl] = useForms();
-    const [pw, onChangePw] = useForms();
+  const [title, onChangeTitle] = useForms();
+  const [date, onChangeDate] = useForms();
+  const [name, onChangeName] = useForms();
+  const [genre, onChangeGenre] = useForms();
+  const [url, onChangeUrl] = useForms();
+  const [pw, onChangePw] = useForms();
 
-    const { logout } = useAuth();
+  const { logout } = useAuth();
 
-    const isFormComplete = title && date && name && url && pw;
+  const isFormComplete = title && date && name && url && pw;
 
-    const onClickUpload = async () => {
-        const accessToken = localStorage.getItem('access')
-        const refreshToken = localStorage.getItem('refresh')
+  const onClickUpload = async () => {
+    const accessToken = localStorage.getItem('access');
+    const refreshToken = localStorage.getItem('refresh');
 
-        try {
-            const res = await normalAPI.post(
-                `/criticismLog`,
-                {
-                    "contentTitle": title,
-                    "localDate": date,
-                    "userName": name,
-                    "genre": genre,
-                    "cloverNoteUrl": url,
-                    "cloverNotePassword": pw
-                }, {
-                headers: {
-                    'accessToken': accessToken
-                }
-            })
-            alert('합평회 기록 등록에 성공했습니다.')
-        } catch (error) {
-            if (error.response && error.response.status === 403) {
-                console.log('토큰 재전송');
-                // Access Token이 만료되었으므로, Refresh Token으로 새로운 Access Token을 발급받는다.
-                try {
-                    const tokenResponse = await normalAPI.post(
-                        `/criticismLog`,
-                        {
-                            "contentTitle": title,
-                            "localDate": date,
-                            "userName": name,
-                            "genre": genre,
-                            "cloverNoteUrl": url,
-                            "cloverNotePassword": pw
-                        },
-                        {
-                            headers: {
-                                'refreshToken': refreshToken,
-                            }
-                        }
-                    );
-                    console.log(tokenResponse);
-                    if (tokenResponse.status === 200) {
-                        const accessToken = tokenResponse.headers.accesstoken.replace('Bearer ', '')
-                        localStorage.setItem('access', accessToken)
-                        if (tokenResponse.headers.refreshtoken) {
-                            const refreshToken = tokenResponse.headers.refreshtoken.replace('Bearer ', '');
-                            localStorage.setItem('refresh', refreshToken);
-                        }
-                        alert('합평회 기록 등록에 성공했습니다.')
-                    }
-                } catch (err) {
-                    console.error('Refresh Token Error:', err);
-                    alert('로그인 세션이 만료되었습니다. 다시 로그인해주세요.');
-                    logout();
-                }
-            } else {
-                console.error('Error:', error);
-                alert('등록 중 문제가 발생했습니다.');
-            }
+    try {
+      const res = await normalAPI.post(
+        `/criticismLog`,
+        {
+          contentTitle: title,
+          localDate: date,
+          userName: name,
+          genre: genre,
+          cloverNoteUrl: url,
+          cloverNotePassword: pw,
+        },
+        {
+          headers: {
+            accessToken: accessToken,
+          },
         }
-
-
-
+      );
+      alert('합평회 기록 등록에 성공했습니다.');
+    } catch (error) {
+      if (error.response && error.response.status === 403) {
+        console.log('토큰 재전송');
+        // Access Token이 만료되었으므로, Refresh Token으로 새로운 Access Token을 발급받는다.
+        try {
+          const tokenResponse = await normalAPI.post(
+            `/criticismLog`,
+            {
+              contentTitle: title,
+              localDate: date,
+              userName: name,
+              genre: genre,
+              cloverNoteUrl: url,
+              cloverNotePassword: pw,
+            },
+            {
+              headers: {
+                refreshToken: refreshToken,
+              },
+            }
+          );
+          console.log(tokenResponse);
+          if (tokenResponse.status === 200) {
+            const accessToken = tokenResponse.headers.accesstoken.replace(
+              'Bearer ',
+              ''
+            );
+            localStorage.setItem('access', accessToken);
+            if (tokenResponse.headers.refreshtoken) {
+              const refreshToken = tokenResponse.headers.refreshtoken.replace(
+                'Bearer ',
+                ''
+              );
+              localStorage.setItem('refresh', refreshToken);
+            }
+            alert('합평회 기록 등록에 성공했습니다.');
+          }
+        } catch (err) {
+          console.error('Refresh Token Error:', err);
+          alert('로그인 세션이 만료되었습니다. 다시 로그인해주세요.');
+          logout();
+        }
+      } else {
+        console.error('Error:', error);
+        alert('등록 중 문제가 발생했습니다.');
+      }
     }
+  };
 
-    return (
-        <BookInfoContainer>
-            <BookTitle>
-                합평회 기록 등록
-            </BookTitle>
+  return (
+    <BookInfoContainer>
+      <BookTitle>합평회 기록 등록</BookTitle>
+      <br />
+      <br />
+      <CriticLogUploads>
+        <div>
+          작품명
+          <br />
+          <LoginInput onChange={onChangeTitle} value={title} />
+        </div>
+        <CriticLogUploadsSecondLine>
+          <div>
+            작가 이름 <br />
+            <LoginInput onChange={onChangeName} value={name} />
+          </div>
+          <div>
+            합평 진행 날짜
             <br />
+            <LoginInput type="date" onChange={onChangeDate} value={date} />
+          </div>
+          <div>
+            장르
             <br />
-            <CriticLogUploads>
-                <div>
-                    작품명<br />
-                    <LoginInput onChange={onChangeTitle} value={title} />
-                </div>
-                <CriticLogUploadsSecondLine>
-                    <div>
-
-                        작가 이름 <br />
-                        <LoginInput onChange={onChangeName} value={name} />
-                    </div>
-                    <div>
-                        합평 진행 날짜
-                        <br />
-                        <LoginInput type='date' onChange={onChangeDate} value={date} />
-                    </div>
-                    <div>
-                        장르
-                        <br />
-                        <VisibleSelect onChange={onChangeGenre} value={genre}>
-                            <option value='NOVEL'>소설</option>
-                            <option value='ESSAY'>수필</option>
-                            <option value='POEM'>시</option>
-                        </VisibleSelect>
-                    </div>
-                </CriticLogUploadsSecondLine>
-                <div>
-                    합평회 접속 링크 <br />
-                    <LoginInput onChange={onChangeUrl} value={url} />
-                </div>
-                <div>
-                    링크 비밀번호 <br />
-                    <LoginInput onChange={onChangePw} value={pw} />
-                </div>
-                <Button disabled={!isFormComplete} onClick={onClickUpload}>등록하기</Button>
-            </CriticLogUploads>
-            <br />
-            <br />  <br />
-            <br />  <br />
-            <br />
-        </BookInfoContainer>
-    );
+            <VisibleSelect onChange={onChangeGenre} value={genre}>
+              <option>장르</option>
+              <option value="NOVEL">소설</option>
+              <option value="ESSAY">수필</option>
+              <option value="POEM">시</option>
+            </VisibleSelect>
+          </div>
+        </CriticLogUploadsSecondLine>
+        <div>
+          합평회 접속 링크 <br />
+          <LoginInput onChange={onChangeUrl} value={url} />
+        </div>
+        <div>
+          링크 비밀번호 <br />
+          <LoginInput onChange={onChangePw} value={pw} />
+        </div>
+        <Button disabled={!isFormComplete} onClick={onClickUpload}>
+          등록하기
+        </Button>
+      </CriticLogUploads>
+      <br />
+      <br /> <br />
+      <br /> <br />
+      <br />
+    </BookInfoContainer>
+  );
 };
 
 export default CriticLogUpload;
--- geulsam/src/pages/work/upload/uploadWork.jsx
+++ geulsam/src/pages/work/upload/uploadWork.jsx
@@ -1,5 +1,18 @@
 import { B, CalendarTitle } from '../../../style/StyledComponent';
-import { Container, EditorContainer, FileInput, GenreButton, GenreContainer, SentenceContainer, SentenceInput, TitleContainer, TitleInput, UploadButton, VisibleSelect, WorkUploadContainer } from '../../../style/WokrUpload';
+import {
+  Container,
+  EditorContainer,
+  FileInput,
+  GenreButton,
+  GenreContainer,
+  SentenceContainer,
+  SentenceInput,
+  TitleContainer,
+  TitleInput,
+  UploadButton,
+  VisibleSelect,
+  WorkUploadContainer,
+} from '../../../style/WokrUpload';
 import { useForms } from '../../../hooks/useForms';
 import { useRef, useState, useEffect } from 'react';
 import Editor from '../../../components/Editor/CKEditor';
@@ -8,30 +21,30 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../store/Auth';
 
 const UploadWork = () => {
-    const [title, onChangeTitle] = useForms();
-    const [genre, setGenre] = useState('');
-    const [file, setFile] = useState(null);
-    const [isVisible, onChangeIsVisible] = useForms();
-    const [sentence, onChangeSentence] = useForms();
-    const [htmlContent, setHtmlContent] = useState(''); // CKEditor의 HTML 내용을 저장할 상태
-    const inputRef = useRef(null);
+  const [title, onChangeTitle] = useForms();
+  const [genre, setGenre] = useState('');
+  const [file, setFile] = useState(null);
+  const [isVisible, onChangeIsVisible] = useForms();
+  const [sentence, onChangeSentence] = useForms();
+  const [htmlContent, setHtmlContent] = useState(''); // CKEditor의 HTML 내용을 저장할 상태
+  const inputRef = useRef(null);
 
-    const { logout } = useAuth();
+  const { logout } = useAuth();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    //업로드 성공 시 workId를 저장해 해당 페이지로 리다이렉트
-    const [workId, setWorkId] = useState('')
+  //업로드 성공 시 workId를 저장해 해당 페이지로 리다이렉트
+  const [workId, setWorkId] = useState('');
 
-    useEffect(() => {
-        if (workId) {
-            navigate(`/work/${workId}`);
-        }
-    }, [workId, navigate]);
+  useEffect(() => {
+    if (workId) {
+      navigate(`/work/${workId}`);
+    }
+  }, [workId, navigate]);
 
-    const handleEditorChange = (data) => {
-        // HTML 문자열에 <meta charset="UTF-8"> 추가하기
-        const htmlContentWithMeta = `
+  const handleEditorChange = (data) => {
+    // HTML 문자열에 <meta charset="UTF-8"> 추가하기
+    const htmlContentWithMeta = `
             <!DOCTYPE html>
             <html lang="ko">
             <head>
@@ -44,160 +57,179 @@ const UploadWork = () => {
             </body>
             </html>
         `;
-        setHtmlContent(htmlContentWithMeta);
-    };
+    setHtmlContent(htmlContentWithMeta);
+  };
 
-    const handleSentenceContainerClick = () => {
-        if (inputRef.current) {
-            inputRef.current.focus();
-        }
-    };
+  const handleSentenceContainerClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
 
-    const handleGenre = (genre) => {
-        setGenre(genre);
-    };
+  const handleGenre = (genre) => {
+    setGenre(genre);
+  };
 
-    const handleButtonClick = () => {
-        document.getElementById('fileInput').click();
-    };
+  const handleButtonClick = () => {
+    document.getElementById('fileInput').click();
+  };
 
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            setFile(file);
-        }
-    };
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFile(file);
+    }
+  };
 
-    const handleSubmit = async () => {
-        const accesstoken = localStorage.getItem('access')
-        const refreshToken = localStorage.getItem('refresh')
+  const handleSubmit = async () => {
+    const accesstoken = localStorage.getItem('access');
+    const refreshToken = localStorage.getItem('refresh');
 
-        if (!file) {
-            alert('PDF 파일을 선택해주세요.');
-            return;
-        }
+    if (!file) {
+      alert('PDF 파일을 선택해주세요.');
+      return;
+    }
 
-        const htmlBlob = new Blob([new TextEncoder().encode(htmlContent)], { type: 'text/html; charset=UTF-8' });
+    const htmlBlob = new Blob([new TextEncoder().encode(htmlContent)], {
+      type: 'text/html; charset=UTF-8',
+    });
 
-        // Blob 객체를 File 객체로 변환
-        const htmlFile = new File([htmlBlob], 'content.html', { type: 'text/html; charset=UTF-8' });
+    // Blob 객체를 File 객체로 변환
+    const htmlFile = new File([htmlBlob], 'content.html', {
+      type: 'text/html; charset=UTF-8',
+    });
 
-        const formData = new FormData();
-        formData.append('name', title);
-        formData.append('pdf', file);
-        formData.append('html', htmlFile); // .html 파일로 변환된 HTML 내용 추가
-        formData.append('genre', genre);
-        formData.append('isVisible', isVisible); // 예시로 'EVERY' 값을 사용
-        formData.append('bookPage', 1); // 예시로 1페이지로 설정
-        formData.append('sentence', sentence);
+    const formData = new FormData();
+    formData.append('name', title);
+    formData.append('pdf', file);
+    formData.append('html', htmlFile); // .html 파일로 변환된 HTML 내용 추가
+    formData.append('genre', genre);
+    formData.append('isVisible', isVisible); // 예시로 'EVERY' 값을 사용
+    formData.append('bookPage', 1); // 예시로 1페이지로 설정
+    formData.append('sentence', sentence);
 
+    try {
+      const response = await normalAPI.post('/content', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          accessToken: accesstoken,
+        },
+      });
+      console.log('서버 응답:', response.data);
+      if (response.data.status === 200) {
+        setWorkId(response.data.data);
+        alert('글을 성공적으로 게시했습니다.');
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 403) {
+        console.log('토큰 재전송');
+        // Access Token이 만료되었으므로, Refresh Token으로 새로운 Access Token을 발급받는다.
         try {
-            const response = await normalAPI.post('/content', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    'accessToken': accesstoken,
-                },
-            });
-            console.log('서버 응답:', response.data);
-            if (response.data.status === 200) {
-                setWorkId(response.data.data)
-                alert('글을 성공적으로 게시했습니다.')
+          const tokenResponse = await normalAPI.post('/content', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+              refreshToken: refreshToken,
+            },
+          });
+          console.log(tokenResponse);
+          if (tokenResponse.status === 200) {
+            const accessToken = tokenResponse.headers.accesstoken.replace(
+              'Bearer ',
+              ''
+            );
+            localStorage.setItem('access', accessToken);
+            if (tokenResponse.headers.refreshtoken) {
+              const refreshToken = tokenResponse.headers.refreshtoken.replace(
+                'Bearer ',
+                ''
+              );
+              localStorage.setItem('refresh', refreshToken);
             }
-
-        } catch (error) {
-            if (error.response && error.response.status === 403) {
-                console.log('토큰 재전송');
-                // Access Token이 만료되었으므로, Refresh Token으로 새로운 Access Token을 발급받는다.
-                try {
-                    const tokenResponse = await normalAPI.post(
-                        '/content',
-                        formData,
-                        {
-                            headers: {
-                                'Content-Type': 'multipart/form-data',
-                                'refreshToken': refreshToken,
-                            }
-                        }
-                    );
-                    console.log(tokenResponse);
-                    if (tokenResponse.status === 200) {
-                        const accessToken = tokenResponse.headers.accesstoken.replace('Bearer ', '')
-                        localStorage.setItem('access', accessToken)
-                        if (tokenResponse.headers.refreshtoken) {
-                            const refreshToken = tokenResponse.headers.refreshtoken.replace('Bearer ', '');
-                            localStorage.setItem('refresh', refreshToken);
-                        }
-                        setWorkId(tokenResponse.data.data)
-                        alert('글을 성공적으로 게시했습니다.')
-                    }
-                } catch (err) {
-                    console.error('Refresh Token Error:', err);
-                    alert('로그인 세션이 만료되었습니다. 다시 로그인해주세요.');
-                    logout();
-                }
-            } else {
-                console.error('Error:', error);
-                alert('게시 중 문제가 발생했습니다.');
-            }
+            setWorkId(tokenResponse.data.data);
+            alert('글을 성공적으로 게시했습니다.');
+          }
+        } catch (err) {
+          console.error('Refresh Token Error:', err);
+          alert('로그인 세션이 만료되었습니다. 다시 로그인해주세요.');
+          logout();
         }
-    };
+      } else {
+        console.error('Error:', error);
+        alert('게시 중 문제가 발생했습니다.');
+      }
+    }
+  };
 
-    return (
-        <WorkUploadContainer>
-            <CalendarTitle>작품 게시하기</CalendarTitle>
-            <Container>
-                <TitleContainer>
-                    <TitleInput value={title} onChange={onChangeTitle} placeholder="제목" />
-                    <VisibleSelect value={isVisible} onChange={onChangeIsVisible}>
-                        <option value="EVERY">전체 공개</option>
-                        <option value="LOGGEDIN">회원 공개</option>
-                        <option value="PRIVATE">비공개</option>
-                    </VisibleSelect>
-                </TitleContainer>
-                <GenreContainer>
-                    작품의 장르와 키워드를 선택합니다.
-                    <br />
-                    <GenreButton onClick={() => handleGenre('NOVEL')} disabled={genre === 'NOVEL'}>
-                        소설
-                    </GenreButton>
-                    <GenreButton onClick={() => handleGenre('POEM')} disabled={genre === 'POEM'}>
-                        시
-                    </GenreButton>
-                    <GenreButton onClick={() => handleGenre('ESSAY')} disabled={genre === 'ESSAY'}>
-                        수필
-                    </GenreButton>
-                </GenreContainer>
-                <GenreContainer>
-                    <B>PDF 파일</B>
-                    <br />
-                    <input
-                        id="fileInput"
-                        type="file"
-                        accept=".pdf"
-                        onChange={handleFileChange}
-                        style={{ display: 'none' }}
-                    />
-                    <FileInput onClick={handleButtonClick}>파일 찾기</FileInput>
-                    {file && <B>{file.name}</B>}
-                </GenreContainer>
-                <SentenceContainer onClick={handleSentenceContainerClick}>
-                    <SentenceInput
-                        ref={inputRef}
-                        rows="2"
-                        cols="40"
-                        value={sentence}
-                        onChange={onChangeSentence}
-                        placeholder="작품 미리보기 분량을 입력해주세요. (최대 130자)"
-                        maxLength="130"
-                    />
-                </SentenceContainer>
-                <EditorContainer>
-                    <Editor onChange={handleEditorChange} /> {/* Editor의 onChange 핸들러에 상태 업데이트 함수 연결 */}
-                </EditorContainer>
-                <UploadButton onClick={handleSubmit}>게시하기</UploadButton>
-            </Container>
-        </WorkUploadContainer>
-    );
+  return (
+    <WorkUploadContainer>
+      <CalendarTitle>작품 게시하기</CalendarTitle>
+      <Container>
+        <TitleContainer>
+          <TitleInput
+            value={title}
+            onChange={onChangeTitle}
+            placeholder="제목"
+          />
+          <VisibleSelect value={isVisible} onChange={onChangeIsVisible}>
+            <option>공개 범위</option>
+            <option value="LOGGEDIN">회원 공개</option>
+            <option value="PRIVATE">비공개</option>
+          </VisibleSelect>
+        </TitleContainer>
+        <GenreContainer>
+          작품의 장르와 키워드를 선택합니다.
+          <br />
+          <GenreButton
+            onClick={() => handleGenre('NOVEL')}
+            disabled={genre === 'NOVEL'}
+          >
+            소설
+          </GenreButton>
+          <GenreButton
+            onClick={() => handleGenre('POEM')}
+            disabled={genre === 'POEM'}
+          >
+            시
+          </GenreButton>
+          <GenreButton
+            onClick={() => handleGenre('ESSAY')}
+            disabled={genre === 'ESSAY'}
+          >
+            수필
+          </GenreButton>
+        </GenreContainer>
+        <GenreContainer>
+          <B>PDF 파일</B>
+          <br />
+          <input
+            id="fileInput"
+            type="file"
+            accept=".pdf"
+            onChange={handleFileChange}
+            style={{ display: 'none' }}
+          />
+          <FileInput onClick={handleButtonClick}>파일 찾기</FileInput>
+          {file && <B>{file.name}</B>}
+        </GenreContainer>
+        <SentenceContainer onClick={handleSentenceContainerClick}>
+          <SentenceInput
+            ref={inputRef}
+            rows="2"
+            cols="40"
+            value={sentence}
+            onChange={onChangeSentence}
+            placeholder="작품 미리보기 분량을 입력해주세요. (최대 130자)"
+            maxLength="130"
+          />
+        </SentenceContainer>
+        <EditorContainer>
+          <Editor onChange={handleEditorChange} />{' '}
+          {/* Editor의 onChange 핸들러에 상태 업데이트 함수 연결 */}
+        </EditorContainer>
+        <UploadButton onClick={handleSubmit}>게시하기</UploadButton>
+      </Container>
+    </WorkUploadContainer>
+  );
 };
 
 export default UploadWork;
